c06e75a489f183fcb39230487d5b9effd7cdefb0
csernakakos <csernakakos@gmail.com>
2022-07-07 21:53:03 +0200

    Removed own parseBody() middleware and replaced it with express.urlencoded()

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
 import express from "express";
 
-function parseBody(req, res, next) {
-    // Let's re-create the functionality of express.urlencoded() below:
-    // req.on() is similar to addEventListener: it fires when, in our case, data in chunks (that is, the HTTP request header in chunks and later on the HTTP request body in chunks) reaches the server.
-    if (req.method === "POST") {
-        console.log("Hello from parseBody() middleware...");
-        req.on("data", data => {
-            console.log(data);
-            const parsedData = data.toString("utf8").split("&");
-            const formData = {};
+// function parseBody(req, res, next) {
+//     // Let's re-create the functionality of express.urlencoded() below:
+//     // req.on() is similar to addEventListener: it fires when, in our case, data in chunks (that is, the HTTP request header in chunks and later on the HTTP request body in chunks) reaches the server.
+//     if (req.method === "POST") {
+//         console.log("Hello from parseBody() middleware...");
+//         req.on("data", data => {
+//             console.log(data);
+//             const parsedData = data.toString("utf8").split("&");
+//             const formData = {};
     
-            for (let pair of parsedData) {
-                // const key = pair.split("=")[0];
-                // const value = pair.split("=")[1];
-                const [ key, value ] = pair.split("=");
+//             for (let pair of parsedData) {
+//                 // const key = pair.split("=")[0];
+//                 // const value = pair.split("=")[1];
+//                 const [ key, value ] = pair.split("=");
     
-                formData[key] = value;
-            };
-            req.body = formData;
-            next();
-        });  
-    } else {
-        next();
-    }
-}
+//                 formData[key] = value;
+//             };
+//             req.body = formData;
+//             next();
+//         });  
+//     } else {
+//         next();
+//     }
+// }
 
 const app = express();
+// app.use(parseBody);
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.send(`
@@ -40,8 +42,8 @@ app.get("/", (req, res) => {
     `)
 });
 
-app.post("/", parseBody, (req, res) => {
-    console.log(req.body, "<<< from middleware")
+app.post("/", (req, res) => {
+    console.log(req.body, "<<< from urlencoded")
     res.send(`
     <p>You submitted ${req.body.email}</p>
     `)
