77bf01a9d9de88a01618df6405292d0b582333af
csernakakos <csernakakos@gmail.com>
2022-07-07 20:49:54 +0200

    express.urlencoded()

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("hello there!");
+import express from "express";
+
+const app = express();
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+    res.send(`
+        <div>
+            <form method="POST">
+                <input name="email" placeholder="email" />
+                <input name="password" placeholder="password" />
+                <input name="passwordConfirmation" placeholder="password confirmation" />
+                <button>Sign Up</button>
+            </form>
+        </div>
+    `)
+});
+
+app.post("/", (req, res) => {
+    console.log("POSTING FORM...");
+    console.log(req.params);
+    console.log(req.body);
+
+    res.send(`
+    <p>You submitted ${req.body.email}</p>
+    `)
+})
+
+app.listen(5001, () => {});
