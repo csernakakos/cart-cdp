a913cd5690d964326bc9f96c723e52d50398ced5
csernakakos <csernakakos@gmail.com>
2022-07-08 07:10:45 +0200

    filename constructor

--- repositories/users.js
+++ repositories/users.js
@@ -1,9 +1,22 @@
+import fs from "fs";
+
 class usersRepository {
     constructor(filename) {
         if (!filename) {
             throw new Error("Creating a repository required a file name.");
         }
+        this.filename = filename;
+        try {
+            fs.accessSync(this.filename);
+        } catch (error) {
+            fs.writeFileSync(this.filename, "[]");
+        }
     }
+
+    async checkForFile() {
+
+    }
+
 }
 
-new usersRepository("a.json");
+const repo = new usersRepository("users.json");
