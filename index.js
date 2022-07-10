e1815054c160227df3ac69a85e7021f8b1d75d5b
csernakakos <csernakakos@gmail.com>
2022-07-10 11:10:12 +0200

    Finished hashing, salting user password

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
+import cookieSession from "cookie-session";
 import usersRepo from "./repositories/users.js";
+
+dotenv.config({});
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieSession({
+    keys: [process.env.COOKIE_KEY],
+}))
 
-app.get("/", (req, res) => {
+app.get("/signup", (req, res) => {
     res.send(`
         <div>
+            Your userId is ${req.session.userId}
             <form method="POST">
                 <input name="email" placeholder="email" />
                 <input name="password" placeholder="password" />
@@ -17,7 +25,7 @@ app.get("/", (req, res) => {
     `)
 });
 
-app.post("/", async (req, res) => {
+app.post("/signup", async (req, res) => {
     const { email, password, passwordConfirmation } = req.body;
 
     const existingUser = await usersRepo.getOneBy({ email });
@@ -30,11 +38,55 @@ app.post("/", async (req, res) => {
         return res.send(`Ensure that the passwords match.`)
     }
 
-    await usersRepo.create({ email, password, passwordConfirmation });
+
+    const user = await usersRepo.create({ email, password });
+    
+    req.session.userId = user.id;
 
     res.send(`
     <p>You've created an account for: ${email}.</p>
     `)
+});
+
+app.get("/signout", async (req, res) => {
+    req.session = null;
+    res.send("You are now logged out.");
+});
+
+app.get("/signin", (req, res) => {
+    res.send(`
+    <div>
+        <form method="POST">
+            <input name="email" placeholder="email" />
+            <input name="password" placeholder="password" />
+            <button>Sign In</button>
+        </form>
+    </div>
+    `)
+});
+
+app.post(`/signin`, async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await usersRepo.getOneBy({ email });
+
+    
+    if (!user) {
+        return res.send("Email not found.");
+    };
+
+    const validPassword = await usersRepo.comparePasswords(user.password, password);
+
+    if (!validPassword) {
+        return res.send("Wrong password.");
+    };
+
+    req.session.userId = user.id;
+
+    res.send(`You're signed in! Your ID is ${req.session.userId}`);
+
 })
+
+
 
 app.listen(5001, () => {});
--- repositories/users.js
+++ repositories/users.js
@@ -1,5 +1,7 @@
 import fs from "fs";
 import crypto from "crypto";
+import util from "util";
+const scrypt = util.promisify(crypto.scrypt);
 
 class usersRepository {
     // constructor function is called instantly when a new instance of a class is created.
@@ -25,10 +27,40 @@ class usersRepository {
 
     async create(attributes) {
         attributes.id = this.randomId();
+
+        // Generate salt, hashedPassword
+        const salt = crypto.randomBytes(8).toString("hex");
+        const buffer = await scrypt(attributes.password, salt, 64);
+
         const records = await this.getAll();
-        records.push(attributes);
+
+        // Replace the default password inside attributes with the buffer and the salt
+        const record = {
+            ...attributes,
+            password: `${buffer.toString("hex")}.${salt}`,
+        };
+
+        records.push(record);
 
         await this.writeAll(records);
+
+        return record;
+    }
+
+    async comparePasswords(saved, entered) {
+     // saved = password stored in database
+     // entered = password entered by user when attempting to sign in   
+
+
+    //  const result = saved.split(".");
+    //  const hashed = result[0];
+    //  const salt = result[1];
+
+     const [ hashed, salt ] = saved.split(".");
+
+     const hashedEnteredBuffer = await scrypt(entered, salt, 64);
+
+     return hashed === hashedEnteredBuffer.toString("hex");
     }
 
     async writeAll(records) {
